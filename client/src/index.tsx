571cd6e6ecd4e15bf56d532280e1d4422442c3a0
atgreen10 <35249183+atgreen10@users.noreply.github.com>
2023-09-07 19:02:08 -0500

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,21 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
 
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'http://localhost:4000',
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+    <React.StrictMode>
+    <ApolloProvider client = {client}>
     <GlobalStyles />
-    <Pages />
+            <Pages />
+    </ApolloProvider>  
   </React.StrictMode>,
   document.getElementById('root')
 );
+
